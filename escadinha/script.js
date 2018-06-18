22130e6fad939c4e5040261eb2e879028adb6d7c
gabriel <gappprp@gmail.com>
2018-06-18 12:05:21 -0300

    function escadinha

--- escadinha/script.js
+++ escadinha/script.js
@@ -1,10 +1,26 @@
-let tamanho = Number(prompt("qual tamanho?"));
-let contador = 0;
-let escadinha = "";
-let degrau = prompt("qual degrau?");
+/*// let tamanho = Number(prompt("qual tamanho?"));
+// let contador = 0;
+// let escadinha = "";
+// let degrau = prompt("qual degrau?");
 
-while(contador < tamanho){
-    escadinha = escadinha = + degrau;
-    console.log(escadinha);
-    contador++;
+// while(contador < tamanho){
+//     escadinha = escadinha = + degrau;
+//     console.log(escadinha);
+//     contador++;
+// }*/
+
+
+function escada(degrau="#", tamanho=10) {
+    let escadinha = "";  contador = 0;  
+
+    while (contador < tamanho){
+        escadinha = escadinha + degrau;
+        console.log(escadinha);
+        contador++;
+        
+    }
+
+    return escadinha;
 }
+
+escada("@", 20);
